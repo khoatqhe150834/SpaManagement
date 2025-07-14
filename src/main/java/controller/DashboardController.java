deca257cfecad374b18b6e07147f8d110cc6a014
CongDat2003 <datvche173158@fpt.edu.vn>
2025-07-15 01:51:51 +0700

    Refactor import statements in DashboardController to remove redundancy

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -1,11 +1,12 @@
 package controller;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import model.Customer;
 import model.User;
 
